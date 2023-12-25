a3eead4e43289014c2b64f7c3212cd6ab841edfc
hieu <mauhieu12@gmail.com>
2023-12-26 01:06:36 +0700

    Add thousandSep and thousandthSep macro

--- src/tera-research/mathfield-macros.ts
+++ src/tera-research/mathfield-macros.ts
@@ -3,91 +3,98 @@ import {MetricConversionsData} from "./MetricConversion.data";
 
 
 export const MATHFIELD_MACTOS_EXTEND = {
-    exponentialE: '\\footnotesize\\textbf×10',
-    modulo: '\\textrm{mod}',
-    npr: '\\operatorname{P}',
-    ncr: '\\operatorname{C}',
-    arcsin: '\\sin^{-1}',
-    arccos: '\\cos^{-1}',
-    arctan: '\\tan^{-1}',
-    arccot: '\\cot^{-1}',
-    outpolar: '\\mathbf{▸r∠φ}',
-    outcomplex: '\\mathbf{▸a+bi}',
+  exponentialE: '\\footnotesize\\textbf×10',
+  modulo: '\\textrm{mod}',
+  npr: '\\operatorname{P}',
+  ncr: '\\operatorname{C}',
+  arcsin: '\\sin^{-1}',
+  arccos: '\\cos^{-1}',
+  arctan: '\\tan^{-1}',
+  arccot: '\\cot^{-1}',
+  outpolar: '\\mathbf{▸r∠φ}',
+  outcomplex: '\\mathbf{▸a+bi}',
 
-    eulerE: '\\mathbf{e}',
+  eulerE: '\\mathbf{e}',
 
-    variable: {
-        args: 1,
-        def: '#1',
-        isImplicitArg: true,
-        argsMapping: variableMapping()
-    },
-    constant: {
-        args: 1,
-        def: '\\mathbf{#1}',
-        isImplicitArg: true,
-        argsMapping: constantMapping()
-    },
-    conversion: {
-        args: 1,
-        def: '\\mathbf{#1}',
-        isImplicitArg: true,
-        argsMapping: conversionMapping()
-    },
+  variable: {
+    args: 1,
+    def: '#1',
+    isImplicitArg: true,
+    argsMapping: variableMapping()
+  },
+  constant: {
+    args: 1,
+    def: '\\mathbf{#1}',
+    isImplicitArg: true,
+    argsMapping: constantMapping()
+  },
+  conversion: {
+    args: 1,
+    def: '\\mathbf{#1}',
+    isImplicitArg: true,
+    argsMapping: conversionMapping()
+  },
 
-    quotient: { //÷R
-        def: '\\textrm{÷R}'
-    },
-    mixfraction: {
-        args: 3,
-        def: '{#1}\\frac{#2}{#3}',
-        isImplicitArg: true,
-    },
-    degree: {
-        def: '^{\\circ}',
-        isImplicitArg: true
-    },
-    minute: {
-        def: '\\,{}^\\prime\\;',
-        isImplicitArg: true,
-    },
-    second: {
-        def: '\\,\\doubleprime\\;',
-        isImplicitArg: true,
-    },
-    termseparator: {
-        def: '\\textrm{, }',
-        isImplicitArg: true,
-    },
-    implicitmul: { //implicitMultiplication
-        def: '',
-        isImplicitArg: true,
-    },
-    decimalsep: {
-      def: '.',
-      isImplicitArg: true
-    }
-
+  quotient: { //÷R
+    def: '\\textrm{÷R}'
+  },
+  mixfraction: {
+    args: 3,
+    def: '{#1}\\frac{#2}{#3}',
+    isImplicitArg: true,
+  },
+  degree: {
+    def: '^{\\circ}',
+    isImplicitArg: true
+  },
+  minute: {
+    def: '\\,{}^\\prime\\;',
+    isImplicitArg: true,
+  },
+  second: {
+    def: '\\,\\doubleprime\\;',
+    isImplicitArg: true,
+  },
+  termseparator: {
+    def: '\\textrm{, }',
+    isImplicitArg: true,
+  },
+  implicitmul: { //implicitMultiplication
+    def: '',
+    isImplicitArg: true,
+  },
+  decimalsep: {
+    def: '.',
+    isImplicitArg: true
+  },
+  thousandSep: {
+    def: ' ',
+    isImplicitArg: true
+  },
+  thousandthSep: {
+    def: ' ',
+    isImplicitArg: true
+  }
 }
 
 function variableMapping() {
-    return {
-        'randreal': '\\mathbf{Rand#}'
-    }
+  return {
+    'randreal': '\\mathbf{Rand#}'
+  }
 }
 
 function constantMapping() {
-    let mapping = {}
-    ScientificConstantsData.forEach(c => {
-        mapping[c.code] = c.symbol
-    })
-    return mapping
+  let mapping = {}
+  ScientificConstantsData.forEach(c => {
+    mapping[c.code] = c.symbol
+  })
+  return mapping
 }
 
 function conversionMapping() {
-    let mapping = {}
-    MetricConversionsData.forEach(c => {
-        mapping[c.code] = c.symbol.replace('►', '→')
-    })
-    return mapping
+  let mapping = {}
+  MetricConversionsData.forEach(c => {
+    mapping[c.code] = c.symbol.replace('►', '→')
+  })
+  return mapping
 }
