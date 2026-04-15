9d673b8f26ef1a02b2ca16e265967f0c4d3830de
root <mauhieu12@gmail.com>
2026-04-15 15:15:56 +0700

    fix: add aria-label to keyboard input and standardize string quotes in mathfield macros

--- src/tera-research/mathfield-macros.ts
+++ src/tera-research/mathfield-macros.ts
@@ -1,18 +1,18 @@
 export const MATHFIELD_MACTOS_EXTEND = {
-  exponentialE: " \\textsc{ᴇ}",
-  modulo: "\\textrm{mod}",
-  npr: "\\operatorname{P}",
-  ncr: "\\operatorname{C}",
-  arcsin: "\\sin^{-1}",
-  arccos: "\\cos^{-1}",
-  arctan: "\\tan^{-1}",
-  arccot: "\\cot^{-1}",
-  outpolar: "\\mathbf{▸r∠φ}",
-  outcomplex: "\\mathbf{▸a+bi}",
+  exponentialE: ' \\textsc{ᴇ}',
+  modulo: '\\textrm{mod}',
+  npr: '\\operatorname{P}',
+  ncr: '\\operatorname{C}',
+  arcsin: '\\sin^{-1}',
+  arccos: '\\cos^{-1}',
+  arctan: '\\tan^{-1}',
+  arccot: '\\cot^{-1}',
+  outpolar: '\\mathbf{▸r∠φ}',
+  outcomplex: '\\mathbf{▸a+bi}',
 
   eulerE: {
-    def: "\\mathbf{e}",
-    isImplicitArg: true
+    def: '\\mathbf{e}',
+    isImplicitArg: true,
   },
 
   // variable: {
@@ -35,8 +35,9 @@ export const MATHFIELD_MACTOS_EXTEND = {
   //   argsMapping: conversionMapping(),
   // },
 
-  quotient: { //÷R
-    def: "\\textrm{÷R}"
+  quotient: {
+    //÷R
+    def: '\\textrm{÷R}',
   },
   // mixfraction: {
   //   args: 3,
@@ -44,36 +45,36 @@ export const MATHFIELD_MACTOS_EXTEND = {
   //   isImplicitArg: true,
   // },
   degree: {
-    def: "^{\\circ}",
-    isImplicitArg: true
+    def: '^{\\circ}',
+    isImplicitArg: true,
   },
   minute: {
-    def: "\\,{}^\\prime\\;",
-    isImplicitArg: true
+    def: '\\,{}^\\prime\\;',
+    isImplicitArg: true,
   },
   second: {
-    def: "\\,\\doubleprime\\;",
-    isImplicitArg: true
+    def: '\\,\\doubleprime\\;',
+    isImplicitArg: true,
   },
   termseparator: {
-    def: "\\textrm{, }\\textrm{ }",
-    isImplicitArg: true
+    def: '\\textrm{, }\\textrm{ }',
+    isImplicitArg: true,
   },
   implicitmul: {
     //implicitMultiplication
-    def: "",
-    isImplicitArg: true
+    def: '\\phantom',
+    isImplicitArg: true,
   },
   decimalsep: {
-    def: ".",
-    isImplicitArg: true
+    def: '.',
+    isImplicitArg: true,
   },
   thousandSep: {
-    def: "\\:",
-    isImplicitArg: true
+    def: '\\:',
+    isImplicitArg: true,
   },
   thousandthSep: {
-    def: "\\:",
-    isImplicitArg: true
-  }
+    def: '\\:',
+    isImplicitArg: true,
+  },
 };
